f85f08b14e26a52927802cab0594ae7e06f9a0d4
Denis Carriere <carriere.denis@gmail.com>
2018-09-20 14:33:52 -0400

    Move to src

--- index.ts
+++ index.ts
@@ -1,57 +1,7 @@
-import WebSocket from "ws";
-import { generateReqId, get_actions, get_table_deltas, unlisten } from "./src/utils";
+// EosWebSocket
+import EosWebSocket from "./src/EosWebSocket";
 
-export default class EosWebSocket extends WebSocket {
-  /**
-   * @name EosWebSocket
-   * @example
-   *
-   * const ws = new EosWebSocket("ws://<SERVER>/v1/stream");
-   */
-  constructor(address: string, options: WebSocket.ClientOptions = {perMessageDeflate: false}) {
-    if (!options.origin) { options.origin = "https://github.com/EOS-Nation/eos-websocket"; }
-    super(address, options);
-  }
+// EosWebSocket Types
+export * from "./types/action_trace";
 
-  /**
-   * @example
-   *
-   * ws.unlisten("req123");
-   */
-  public unlisten(req_id: string) {
-    this.send(JSON.stringify(unlisten(req_id)));
-  }
-
-  /**
-   * Get Actions
-   *
-   * @param {string} account Account
-   * @param {string} action_name Action Name
-   * @param {string} [receiver] Receiver
-   * @returns {string} req_id
-   * @example
-   *
-   * ws.get_actions("eosio.token", "transfer");
-   */
-  public get_actions(account: string, action_name: string, receiver?: string) {
-    const req_id = generateReqId();
-    this.send(JSON.stringify(get_actions(req_id, account, action_name, receiver)));
-    return req_id;
-  }
-
-  /**
-   * Get Table Deltas
-   *
-   * @param {string} code Code
-   * @param {string} scope Scope
-   * @param {string} table_name Table Name
-   * @example
-   *
-   * ws.get_table_deltas("eosio", "eosio", "global");
-   */
-  public get_table_deltas(code: string, scope: string, table_name: string) {
-    const req_id = generateReqId();
-    this.send(JSON.stringify(get_table_deltas(req_id, code, scope, table_name)));
-    return req_id;
-  }
-}
+export default EosWebSocket;
