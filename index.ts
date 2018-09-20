6b5907ad583f66781af5724af7963b17e673e84f
Denis Carriere <carriere.denis@gmail.com>
2018-09-20 12:31:02 -0400

    Update packaging

--- index.ts
+++ index.ts
@@ -1,65 +1,53 @@
 import WebSocket from "ws";
+import { generateReqId, get_actions, get_table_deltas, unlisten } from "./src/utils";
 
 export default class EosWebSocket extends WebSocket {
   public req_id: string;
 
   /**
+   * @name EosWebSocket
    * @example
    *
    * const ws = new EosWebSocket("ws://<SERVER>/v1/stream");
    */
-  constructor (address: string, options: WebSocket.ClientOptions = {perMessageDeflate: false}) {
-    if (!options.origin) { options.origin = 'https://github.com/EOS-Nation/eos-websocket'; }
+  constructor(address: string, options: WebSocket.ClientOptions = {perMessageDeflate: false}) {
+    if (!options.origin) { options.origin = "https://github.com/EOS-Nation/eos-websocket"; }
     super(address, options);
     this.req_id = generateReqId();
   }
+  /**
+   * @example
+   *
+   * ws.unlisten();
+   */
   public unlisten() {
     this.send(JSON.stringify(unlisten(this.req_id)));
   }
+
+  /**
+   * Get Actions
+   *
+   * @param {string} account Account
+   * @param {string} action_name Action Name
+   * @param {string} [receiver] Receiver
+   * @example
+   *
+   * ws.get_actions("eosio.token", "transfer");
+   */
   public get_actions(account: string, action_name: string, receiver?: string) {
-    this.send(JSON.stringify(get_actions(account, action_name, receiver, this.req_id)));
+    this.send(JSON.stringify(get_actions(this.req_id, account, action_name, receiver)));
   }
+  /**
+   * Get Table Deltas
+   *
+   * @param {string} code Code
+   * @param {string} scope Scope
+   * @param {string} table_name Table Name
+   * @example
+   *
+   * ws.get_table_deltas("eosio", "eosio", "global");
+   */
   public get_table_deltas(code: string, scope: string, table_name: string) {
-    this.send(JSON.stringify(get_table_deltas(code, scope, table_name, this.req_id)));
-  }
-}
-
-export function generateReqId() {
-  return "req" + Math.round(Math.random() * 1000);
-}
-
-export function unlisten(req_id: string) {
-  return {
-    type: "unlisten",
-    data: {
-      req_id
-    }
-  };
-}
-
-export function get_actions(account: string, action_name: string, receiver?: string, req_id?: string) {
-  return {
-    type: "get_actions",
-    req_id,
-    listen: true,
-    data: {
-      account,
-      action_name,
-      receiver: receiver || account,
-    }
-  }
-}
-
-export function get_table_deltas(code: string, scope: string, table_name: string, req_id?: string) {
-  return {
-    type: "get_table_deltas",
-    req_id,
-    listen: true,
-    data: {
-      code,
-      scope,
-      table_name,
-      json: true
-    }
+    this.send(JSON.stringify(get_table_deltas(this.req_id, code, scope, table_name)));
   }
 }
