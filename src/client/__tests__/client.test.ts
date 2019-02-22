3f87dcf990acf4c67772a4425e9c7e688470be2e
Alexandre Coppens <alexcppns@gmail.com>
2019-02-22 15:38:30 -0500

    test(client): increase test coverage

--- src/client/__tests__/client.test.ts
+++ src/client/__tests__/client.test.ts
@@ -9,30 +9,52 @@ describe("client", function() {
 
   const mockSocket = createMockEoswsSocket()
   const socket = (mockSocket as any) as EoswsSocket
-  const mockFetch = fetch as HttpClient
+  const mockFetch = fetch
   beforeEach(() => {
     receivedMessages = []
   })
 
   describe("constructor", () => {
     it("should build the client", () => {
-      const client = new EoswsClient({ socket, httpClient: mockFetch, baseUrl: "test.io" })
+      const client = new EoswsClient({ socket, httpClient: mockFetch as any, baseUrl: "test.io" })
       expect(client.socket).toBeDefined()
     })
   })
 
   describe("connect", () => {
     it("should add a listener", async () => {
-      const client = new EoswsClient({ socket, httpClient: mockFetch, baseUrl: "test.io" })
-
+      const client = new EoswsClient({ socket, httpClient: mockFetch as any, baseUrl: "test.io" })
       await client.connect()
       expect(client.socket.connect).toHaveBeenCalled()
     })
   })
 
+  describe("reconnect", () => {
+    it("should add a listener", async () => {
+      const client = new EoswsClient({ socket, httpClient: mockFetch as any, baseUrl: "test.io" })
+      client.socket.isConnected = true
+      await client.reconnect()
+      expect(client.socket.disconnect).toHaveBeenCalled()
+      expect(client.socket.connect).toHaveBeenCalled()
+    })
+  })
+
+  describe("getNewApiToken", () => {
+    it("should ", function() {
+      // @ts-ignore
+      mockFetch.mockResponses([JSON.stringify({ token: "token", expires_at: 123456 })])
+      const client = new EoswsClient({ socket, httpClient: mockFetch as any, baseUrl: "test.io" })
+      client.getNewApiToken("abc")
+      expect(mockFetch).toHaveBeenCalledWith("test.io/v1/auth/issue", {
+        method: "post",
+        body: JSON.stringify({ api_key: "abc" })
+      })
+    })
+  })
+
   describe("getActionTraces", () => {
     it("should return the same object as the basic send", () => {
-      const client = new EoswsClient({ socket, httpClient: mockFetch, baseUrl: "test.io" })
+      const client = new EoswsClient({ socket, httpClient: mockFetch as any, baseUrl: "test.io" })
 
       expect(
         client.getActionTraces(
@@ -45,7 +67,7 @@ describe("client", function() {
 
   describe("getTableRows", () => {
     it("should return the same object as the basic send", () => {
-      const client = new EoswsClient({ socket, httpClient: mockFetch, baseUrl: "test.io" })
+      const client = new EoswsClient({ socket, httpClient: mockFetch as any, baseUrl: "test.io" })
 
       expect(
         client.getTableRows({ scope: "test", code: "code", table: "test" }, { req_id: "abc" })
@@ -55,7 +77,7 @@ describe("client", function() {
 
   describe("getTransaction", () => {
     it("should return the same object as the basic send", () => {
-      const client = new EoswsClient({ socket, httpClient: mockFetch, baseUrl: "test.io" })
+      const client = new EoswsClient({ socket, httpClient: mockFetch as any, baseUrl: "test.io" })
 
       expect(client.getTransactionLifecycle("id", { listen: true, req_id: "abc" })).toEqual({
         reqId: "abc",
