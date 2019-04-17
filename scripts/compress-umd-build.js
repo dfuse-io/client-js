d95338cffb8702a54cc60b0583c1419ee1cdcb15
Matthieu Vachon <matt@eoscanada.com>
2019-04-17 16:15:07 -0400

    Added compressed UMD build

---
+++ scripts/compress-umd-build.js
@@ -0,0 +1,29 @@
+const fs = require("fs")
+const stream = require("stream")
+const zlib = require("zlib")
+const util = require("util")
+
+const directory = "dist"
+const filePattern = `${directory}/dfuse-client.umd`
+
+async function main() {
+  await zip(`${filePattern}.js`, `${filePattern}.js.gz`)
+  await zip(`${filePattern}.js.map`, `${filePattern}.js.map.gz`)
+
+  process.exit(0)
+}
+
+async function zip(source, destination) {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(source)
+      .pipe(zlib.createGzip())
+      .pipe(fs.createWriteStream(destination))
+      .on("finish", resolve)
+      .on("error", reject)
+  })
+}
+
+main().catch((error) => {
+  console.error(error.stack)
+  process.exit(1)
+})
